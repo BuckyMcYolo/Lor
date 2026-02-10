54236421eaf7ac83223a2e101a47647b25a25304
Jacob Owens <jacobowens75@gmail.com>
2026-02-10 09:40:29 -0800

    Add protocol to API URL if missing

--- packages/env/src/client.ts
+++ packages/env/src/client.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
+const addProtocol = (url: string) =>
+	url.startsWith("http://") || url.startsWith("https://")
+		? url
+		: `https://${url}`;
+
 const clientSchema = z.object({
-	NEXT_PUBLIC_API_URL: z.string().url(),
+	NEXT_PUBLIC_API_URL: z.string().min(1).transform(addProtocol),
 });
 
 export const env = clientSchema.parse({
